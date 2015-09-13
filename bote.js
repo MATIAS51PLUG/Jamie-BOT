d44d0e822b41a11d38e9589f8f7b1acd0968c817
MATIAS51PLUG <matiasaguileraaburto@gmail.com>
2015-09-12 22:35:51 -0400

    Update bote.js

--- bote.js
+++ bote.js
@@ -204,7 +204,7 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/MATIAS51PLUG/Jamie-BOT/master/bote.js", extend);
 
 }).call(this);
 
